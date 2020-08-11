2146aed35f94e59ceaca50cffd262751b64b6b9a
Jacob Daughtry <akutozo@gmail.com>
2020-08-10 21:27:54 -0500

    Building index file

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+const util = require("util");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
