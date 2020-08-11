f4e536370e62428d14338ad887f2456c03bf8d22
Jacob Daughtry <akutozo@gmail.com>
2020-08-10 19:14:16 -0500

    intern and engineer updates

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require('../lib/Engineer.js');
+
+test("Engineers Github profile", () => {
+    const testValue = "akutozo";
+    const employee = new Engineer("Al Yankovic", 1337, "whiteandnerdy@thechamp.com", testValue);
+    expect(employee.github).toBe(testValue);
+})
+
+test("Setting the role to Engineer", () => {
+    const testValue = "Engineer";
+    const employee = new Engineer("Al Yankovic", 1337, "whiteandnerdy@thechamp.com", testValue);
+    expect(employee.empRole()).toBe(testValue);
+})
+
+test("Grabbing that github profile", () => {
+    const testValue = "akutozo";
+    const employee = new Engineer("Al Yankovic", 1337, "whiteandnerdy@thechamp.com", testValue);
+    expect(employee.getGitHub()).toBe(testValue);
+})
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
-const Intern = require('../lib/Intern.js');
+const Intern = require("../lib/Intern");
+
+test("What School is providing us the sacrifice?", () => {
+    const testValue = "Pennsylvania State University";
+    const employee = new Intern("Faleolo Alailima", 1001, "intern4@thechamp.com", testValue);
+    expect(employee.school).toBe(testValue);
+})
+
+test("getRole function", () => {
+    const testValue = "Intern";
+    const employee = new Intern("Faleolo Alailima", 1001, "intern4@thechamp.com", testValue);
+    expect(employee.empRole()).toBe(testValue);
+})
+
+test("Verifying the interns school", () => {
+    const testValue = "Pennsylvania State University";
+    const employee = new Intern("Faleolo Alailima", 1001, "intern4@thechamp.com", testValue);
+    expect(employee.intSchool()).toBe(testValue);
+});
